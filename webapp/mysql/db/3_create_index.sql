bec3bce8952b2756321e91f357f7b1a02f422b5f
Yuta <yuta@hogehoge.co.jp>
2021-02-01 06:34:33 +0000

    検索条件のインデックスを追加

--- webapp/mysql/db/3_create_index.sql
+++ webapp/mysql/db/3_create_index.sql
@@ -8,3 +8,12 @@ CREATE INDEX rent_index ON estate(rent);
 
 -- order by狙い
 CREATE INDEX price_index ON chair(price);
+
+-- 検索条件にも Index を貼る
+---- ダブリ
+-- ALTER TABLE chair ADD INDEX chair_price_idx(price, stock);
+ALTER TABLE chair ADD INDEX chair_height_idx(height, stock);
+ALTER TABLE chair ADD INDEX chair_kind_idx(kind, stock);
+
+ALTER TABLE estate ADD INDEX estate_rent_door_width_idx(rent, door_width);
+ALTER TABLE estate ADD INDEX estate_rent_door_height_idx(rent, door_height);
